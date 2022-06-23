db06df313aa7fe14d537c2d6e9e24eba72a71a98
Zhuravlev <zhuravlev112358@mail.ru>
2022-06-24 02:48:17 +0300

    feat: completed task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,48 @@
-module.exports = function toReadable (number) {
-  
-}
+module.exports = function toReadable(number) {
+    const units = [
+        "zero",
+        "one",
+        "two",
+        "three",
+        "four",
+        "five",
+        "six",
+        "seven",
+        "eight",
+        "nine",
+        "ten",
+        "eleven",
+        "twelve",
+        "thirteen",
+        "fourteen",
+        "fifteen",
+        "sixteen",
+        "seventeen",
+        "eighteen",
+        "nineteen",
+    ];
+    const dozens = [
+        "twenty",
+        "thirty",
+        "forty",
+        "fifty",
+        "sixty",
+        "seventy",
+        "eighty",
+        "ninety",
+    ];
+
+    if (number < 20) return units[number];
+    const remains = number % 10;
+    if (number < 100)
+        return (
+            dozens[Math.floor(number / 10) - 2] +
+            (remains ? " " + units[remains] : "")
+        );
+    if (number <= 999)
+        return (
+            units[Math.floor(number / 100)] +
+            " hundred" +
+            (number % 100 === 0 ? "" : " " + toReadable(number % 100))
+        );
+};
